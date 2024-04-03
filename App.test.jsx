41285b23fbe35771079598460f0c48a9c957d278
Xyves <xodrixx@protonmail.com>
2024-04-03 20:51:57 +0200

    Second commit

--- App.test.jsx
+++ App.test.jsx
@@ -1,6 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "./src/App";
 
 describe("App component", () => {
   it("renders correct heading", () => {
