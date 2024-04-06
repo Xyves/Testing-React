cc775d50847422947a98f5708c88e078e8d31655
Xyves <xodrixx@protonmail.com>
2024-04-06 19:53:08 +0200

    Add new tests

--- App.test.jsx
+++ App.test.jsx
@@ -1,10 +1,18 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./src/App";
 
-describe("App component", () => {
-  it("renders correct heading", () => {
+describe("App Component", () => {
+  it("renders magnificent monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+  it("Renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
     render(<App />);
-    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+    const button = screen.getByRole("button", { name: "Click me" });
+    await user.click(button);
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
